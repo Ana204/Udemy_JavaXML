7bcf2341a630f02c1b1abc6b4d6112f4d2f4d257
ana204 <anagomes.lucia@hotmail.com>
2021-06-21 18:19:12 -0300

    criando a class cliente

--- PrimeiroNivelamento/app/src/main/java/primeiro/nivelamento/nivelamento/MainActivity.java
+++ PrimeiroNivelamento/app/src/main/java/primeiro/nivelamento/nivelamento/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Cliente cliente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cliente = new Cliente("Ana Lucia", "ana@hotmail.com", "99999999", 20, true);
+
+        System.out.println(cliente);
     }
 }

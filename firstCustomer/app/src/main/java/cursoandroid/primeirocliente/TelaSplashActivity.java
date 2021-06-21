4a5a2f99de1b82d7775ebffd0ef083b67cb81286
ana204 <anagomes.lucia@hotmail.com>
2021-06-21 15:27:30 -0300

    troca de tela e tela splash finalizada

--- firstCustomer/app/src/main/java/cursoandroid/primeirocliente/TelaSplashActivity.java
+++ firstCustomer/app/src/main/java/cursoandroid/primeirocliente/TelaSplashActivity.java
@@ -2,13 +2,31 @@ package cursoandroid.primeirocliente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class TelaSplashActivity extends AppCompatActivity {
+
+    int microseconds = 15000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_splash);
+
+        trocarTela();
+    }
+
+    private void trocarTela() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent trocaDetela = new Intent(TelaSplashActivity.this, MainActivity.class);
+                startActivity(trocaDetela);
+                finish();
+            }
+        }, microseconds);
     }
 }

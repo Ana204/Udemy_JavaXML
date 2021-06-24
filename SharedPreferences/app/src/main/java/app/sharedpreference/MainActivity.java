34cdc6029ab3b4149fdb4c77ae9cd92c9ad7cbdd
ana204 <anagomes.lucia@hotmail.com>
2021-06-24 16:49:04 -0300

    Criando pasta do Shared

--- SharedPreferences/app/src/main/java/app/sharedpreference/MainActivity.java
+++ SharedPreferences/app/src/main/java/app/sharedpreference/MainActivity.java
@@ -2,13 +2,25 @@ package app.sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREF_NOME = "App_sharedPref";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("Rodando !!");
+
+        sharedPreferences = getSharedPreferences(PREF_NOME, Context.MODE_PRIVATE);
+
+        System.out.println("Pasta Criada !!");
+
     }
 }

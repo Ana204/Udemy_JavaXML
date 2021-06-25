63e9e8b417882797fcf69e2514351f479a409be3
ana204 <anagomes.lucia@hotmail.com>
2021-06-25 12:59:07 -0300

    Colocando métodos na interface

---
+++ Interface/app/src/main/java/aula/nivelamentointerface/ICrud.java
@@ -0,0 +1,12 @@
+package aula.nivelamentointerface;
+
+public interface ICrud {
+
+    public void incluir();
+
+    public void alterar();
+
+    public void deletar();
+
+    public void listar();
+}

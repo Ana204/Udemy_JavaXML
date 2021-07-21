b704c351bde2485bd858ebcb0c2adb50863a2a9b
ana204 <anagomes.lucia@hotmail.com>
2021-07-21 09:23:11 -0300

    Declaração das variaveis e iniciando o método

--- GeradorQRcode2/app/src/main/java/app/geradorqrcode/MainActivity.kt
+++ GeradorQRcode2/app/src/main/java/app/geradorqrcode/MainActivity.kt
@@ -2,10 +2,26 @@ package app.geradorqrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    var digiteQrCode: EditText? = null
+    var buttonGerar: Button? = null
+    var imgQRCode: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponentes()
+    }
+
+    fun initComponentes() {
+        digiteQrCode = findViewById(R.id.digiteQrCode)
+        buttonGerar = findViewById(R.id.buttonGerar)
+        imgQRCode = findViewById(R.id.imgQRCode)
     }
 }

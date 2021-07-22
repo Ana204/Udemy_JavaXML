f13fc851cc588702183bf04024f79159a4c46b74
ana204 <anagomes.lucia@hotmail.com>
2021-07-22 14:55:07 -0300

    Usando o LocationManager para acessar a localização

--- CoordenadasGPS/app/src/main/java/app/coordenadasgps/MainActivity.java
+++ CoordenadasGPS/app/src/main/java/app/coordenadasgps/MainActivity.java
@@ -2,13 +2,76 @@ package app.coordenadasgps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView valorLatitude, valorLongitude;
+
+    LocationManager locationManager;
+
+    double latitude, longitude;
+    boolean gpsAtivo = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valorLatitude = findViewById(R.id.valorLatitude);
+        valorLongitude = findViewById(R.id.valorLongitude);
+
+        locationManager = (LocationManager) getApplication().getSystemService(Context.LOCATION_SERVICE);
+
+        gpsAtivo = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(gpsAtivo){
+            obterCoordenadas();
+        }else {
+            latitude = 0.00;
+            longitude = 0.00;
+
+            valorLatitude.setText(String.valueOf(latitude));
+            valorLongitude.setText(String.valueOf(longitude));
+
+            Toast.makeText(this, "ATIVE O SEU GPS", Toast.LENGTH_LONG).show();
+        }
     }
+
+    private void obterCoordenadas() {
+
+        boolean permissaoAtiva = true;
+
+        if (permissaoAtiva){
+
+            ultimaLocalizacaoValida();
+        }else {
+            permissaoParaLocalizacao();
+        }
+
+    }
+
+    private void ultimaLocalizacaoValida() {
+
+        latitude = 1.98;
+        longitude = -1.67;
+
+        valorLatitude.setText(String.valueOf(latitude));
+        valorLongitude.setText(String.valueOf(longitude));
+
+        Toast.makeText(this, "GPS ATIVADO", Toast.LENGTH_LONG).show();
+    }
+
+
+    private void permissaoParaLocalizacao() {
+
+        Toast.makeText(this, "APP SEM PERMISSÃO", Toast.LENGTH_LONG).show();
+
+    }
+
+
 }

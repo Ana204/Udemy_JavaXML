d5a9c46c2acd1dabff824ae863b50977164c574d
ana204 <anagomes.lucia@hotmail.com>
2021-07-26 15:01:04 -0300

    if else

--- nivelamentoKotlin/src/main/kotlin/ControleDeFluxo.kt
+++ nivelamentoKotlin/src/main/kotlin/ControleDeFluxo.kt
@@ -1,4 +1,17 @@
 fun main() {
 
-    println("Controle de fluxo usando if-else e os laços for")
+    var num1 = 10
+    var num2 = 10
+
+    if (num2 == 0){
+
+        println("Impossivel dividir")
+
+    }else{
+
+        var D = num1 / num2
+
+        println(D)
+    }
+ 
 }

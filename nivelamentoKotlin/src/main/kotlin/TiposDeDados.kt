a82e8776d97501eae1767f6e4f48502e02c43e71
ana204 <anagomes.lucia@hotmail.com>
2021-07-26 12:16:08 -0300

    Tipos de dados

---
+++ nivelamentoKotlin/src/main/kotlin/TiposDeDados.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    //tipo String
+    val menssage = "Olá Kotlin"
+    println(menssage)
+
+    val curso = "Kotlin 2021"
+    var preco = 29.99
+    val totalVagas = 50
+    val inicio = "26/07/2021"
+    
+    println("Curso: $curso, Preço: $preco, Total de Vagas: $totalVagas, Inicio: $inicio")
+}

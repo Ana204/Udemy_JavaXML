aeb68db446382db4b1d5b42e06d69eb2a9980246
ana204 <anagomes.lucia@hotmail.com>
2021-07-26 16:01:05 -0300

    Loops

---
+++ nivelamentoKotlin/src/main/kotlin/Loops.kt
@@ -0,0 +1,39 @@
+fun main (){
+
+    //LAÇO FOR
+/*    for (i in 0..9){
+
+        println("$i")
+    }*/
+
+/*    for (i in 9 downTo 0 step 3){
+
+        println("$i")
+    }*/
+//----------------------------------------------------------------------------------------------------
+
+    //LAÇO WHILE (enquanto)
+
+/*    var contador = 0
+
+    while (contador <= 9 ){
+
+        println(contador)
+
+        ++contador
+    }*/
+
+//-----------------------------------------------------------------------------------------------------
+
+    //LAÇO DO WHILE (enquanto for true)
+
+    var contador = 90
+
+    do {
+
+        println(contador)
+
+        ++contador
+
+    }while (contador <= 9)
+}

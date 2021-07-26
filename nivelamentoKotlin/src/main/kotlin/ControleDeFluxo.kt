0c5dd1dd5933cda70ed4b5e96025285988b62908
ana204 <anagomes.lucia@hotmail.com>
2021-07-26 15:09:03 -0300

    if else especificamente kotlin

--- nivelamentoKotlin/src/main/kotlin/ControleDeFluxo.kt
+++ nivelamentoKotlin/src/main/kotlin/ControleDeFluxo.kt
@@ -1,9 +1,10 @@
 fun main() {
 
     var num1 = 10
-    var num2 = 10
+    var num2 = 50
+    var num3 = 0
 
-    if (num2 == 0){
+/*    if (num2 == 0){
 
         println("Impossivel dividir")
 
@@ -12,6 +13,15 @@ fun main() {
         var D = num1 / num2
 
         println(D)
+    }*/
+
+    num3 = if (num1 > num2){
+
+        println("O valor de num3 é: $num1")
+        num1
+    }else{
+        println("O valor de num3 é: $num2")
+        num2
     }
- 
+
 }

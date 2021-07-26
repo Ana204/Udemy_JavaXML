6b04b7626df193f45b3227fdb17482ab5ddd1224
ana204 <anagomes.lucia@hotmail.com>
2021-07-26 15:22:35 -0300

    Usando o When no Kotlin

--- nivelamentoKotlin/src/main/kotlin/ControleDeFluxoWhen.kt
+++ nivelamentoKotlin/src/main/kotlin/ControleDeFluxoWhen.kt
@@ -1,4 +1,20 @@
 fun main(){
 
-    println("Hello World")
+   /* 0 - para adicionar
+      1 - para alterar
+      2 - para deletar
+      3 - sair
+      else - opção invalida */
+
+  var menu = 4
+
+    when(menu){
+
+        0 -> println("ADICIONAR")
+        1 -> println("ALTERAR")
+        2 -> println("DELETAR")
+        3 -> println("SAIR")
+        else -> println("OPÇÃO INVÁLIDA")
+    }
+
 }

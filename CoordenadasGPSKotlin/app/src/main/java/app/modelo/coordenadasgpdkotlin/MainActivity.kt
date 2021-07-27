bddcbc5d52255ef0e878b04c506a95842a3d901b
ana204 <anagomes.lucia@hotmail.com>
2021-07-27 14:52:01 -0300

    array de permissões declaradas

--- CoordenadasGPSKotlin/app/src/main/java/app/modelo/coordenadasgpdkotlin/MainActivity.kt
+++ CoordenadasGPSKotlin/app/src/main/java/app/modelo/coordenadasgpdkotlin/MainActivity.kt
@@ -1,11 +1,42 @@
 package app.modelo.coordenadasgpdkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    var permissoesRequeridas = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("AQUII EM SP"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.setZoomControlsEnabled(true)
+        mMap.setMinZoomPreference(9.5f)
+
     }
 }

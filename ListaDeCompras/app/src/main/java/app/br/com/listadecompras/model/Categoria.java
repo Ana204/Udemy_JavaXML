767d9a868876babe2990b99932b2cd57bb19d5c5
ana204 <anagomes.lucia@hotmail.com>
2021-08-05 16:18:50 -0300

    Criação do banco e da tabela Categoria

---
+++ ListaDeCompras/app/src/main/java/app/br/com/listadecompras/model/Categoria.java
@@ -0,0 +1,25 @@
+package app.br.com.listadecompras.model;
+
+import java.util.List;
+
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Categoria extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+
+    @Required
+    private String nomeDaCategoria;
+
+    private int totalDeProdutos;
+    private byte[] imagemCategoria;
+
+    @Ignore
+    private List<String> tituloCategoriaSpinner;
+    @Ignore
+    private List<Categoria> categorias;
+}

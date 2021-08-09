b958c92233565475f36781ee9517289bab28ddb2
ana204 <anagomes.lucia@hotmail.com>
2021-08-09 14:27:12 -0300

    Método para inserir dados na tabela produto e Categoria

--- ListaDeCompras/app/src/main/java/app/br/com/listadecompras/controller/CategoriaController.java
+++ ListaDeCompras/app/src/main/java/app/br/com/listadecompras/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package app.br.com.listadecompras.controller;
 import java.util.List;
 
 import app.br.com.listadecompras.model.Categoria;
+import io.realm.Realm;
 
 public class CategoriaController implements ICrud<Categoria>{
 
@@ -10,6 +11,18 @@ public class CategoriaController implements ICrud<Categoria>{
     @Override
     public void insert(Categoria obj) {
 
+        Realm realm = Realm.getDefaultInstance();
+
+        Number primaryKey = realm.where(Categoria.class).max("id");
+
+        final int autoIncrementPrimaryKey = (primaryKey == null) ? 1 : primaryKey.intValue() + 1;
+
+        obj.setId(autoIncrementPrimaryKey);
+
+        realm.beginTransaction();
+        realm.copyToRealm(obj);
+        realm.commitTransaction();
+        realm.close();
     }
 
     @Override
--- ListaDeCompras/app/src/main/java/app/br/com/listadecompras/controller/ProdutoController.java
+++ ListaDeCompras/app/src/main/java/app/br/com/listadecompras/controller/ProdutoController.java
@@ -2,13 +2,28 @@ package app.br.com.listadecompras.controller;
 
 import java.util.List;
 
+import app.br.com.listadecompras.model.Categoria;
 import app.br.com.listadecompras.model.Produto;
+import io.realm.Realm;
 
 public class ProdutoController implements ICrud<Produto>{
 
     @Override
     public void insert(Produto obj) {
-        
+
+        Realm realm = Realm.getDefaultInstance();
+
+        Number primaryKey = realm.where(Produto.class).max("id");
+
+        final int autoIncrementPrimaryKey = (primaryKey == null) ? 1 : primaryKey.intValue() + 1;
+
+        obj.setId(autoIncrementPrimaryKey);
+
+        realm.beginTransaction();
+        realm.copyToRealm(obj);
+        realm.commitTransaction();
+        realm.close();
+
     }
 
     @Override

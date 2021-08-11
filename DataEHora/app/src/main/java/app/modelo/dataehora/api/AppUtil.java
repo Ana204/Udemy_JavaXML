e23b40bcff6c18ddb3f79afc3e3af342fd7571ef
ana204 <anagomes.lucia@hotmail.com>
2021-08-11 15:14:59 -0300

    Versão2 dos metodos para pegar data e hora

--- DataEHora/app/src/main/java/app/modelo/dataehora/api/AppUtil.java
+++ DataEHora/app/src/main/java/app/modelo/dataehora/api/AppUtil.java
@@ -1,6 +1,9 @@
 package app.modelo.dataehora.api;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AppUtil {
 
@@ -11,35 +14,15 @@ public class AppUtil {
     public static String getDataAtual(){
 
         String dataAtual = "dd/mm/aaaa";
-        String dia, mes, ano;
 
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyyy");
+        Date data = new Date();
 
-        try {
-
-            Calendar calendar = Calendar.getInstance();
-
-            dia = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-            mes = String.valueOf(calendar.get(Calendar.MONTH)+1);
-            ano = String.valueOf(calendar.get(Calendar.YEAR));
-
-
-//           dia = (Calendar.DAY_OF_MONTH < 10) ? "0"+dia : dia;
-
-            int mesAtual = (Calendar.MONTH)+1;
-
-            mes = (mesAtual < 10) ? "0"+mes : mes;
-
-           dataAtual = dia+"/"+mes+"/"+ano;
-
-            return dataAtual;
-
-        }catch (Exception e){
-
-        }
+        dataAtual = dateFormat.format(data);
 
         return dataAtual;
-    }
 
+    }
 
     /**
      *
@@ -48,33 +31,15 @@ public class AppUtil {
 
     public static String getHoraAtual(){
 
-        String horas, minutos, segundos;
+
 
         String horaAtual = "00:00:00";
 
 
-        try{
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date data = new Date();
 
-            Calendar calendar = Calendar.getInstance();
-
-            horas = String.valueOf(calendar.get(Calendar.HOUR_OF_DAY));
-            minutos = String.valueOf(calendar.get(Calendar.MINUTE));
-            segundos = String.valueOf(calendar.get(Calendar.SECOND));
-
-            horas = (Calendar.HOUR_OF_DAY <= 9) ? "0"+horas : horas;
-            minutos = (Calendar.MINUTE <= 9) ? "0"+minutos : minutos;
-            segundos = (Calendar.MILLISECOND <= 9) ? "0"+segundos : segundos;
-
-            horaAtual = horas+":"+minutos+":"+segundos;
-
-
-            return horaAtual;
-
-
-
-        }catch (Exception e){
-
-        }
+        horaAtual = dateFormat.format(data);
 
         return horaAtual;
     }

e03ed92fd812a8c245e6763d86e6bd8a94766874
ana204 <anagomes.lucia@hotmail.com>
2021-08-12 11:19:05 -0300

    Navegando entre uma tela e outra atraves do button

--- Cadastro/app/src/main/java/app/modelo/cadastro/MainActivity.java
+++ Cadastro/app/src/main/java/app/modelo/cadastro/MainActivity.java
@@ -2,7 +2,10 @@ package app.modelo.cadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import app.modelo.cadastro.api.AppUtil;
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView data;
     TextView hora;
+    Button btnCadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         data = findViewById(R.id.data);
         hora = findViewById(R.id.hora);
+        btnCadastro = findViewById(R.id.btnCadastro);
+
 
         data.setText(AppUtil.getDataAtual());
         hora.setText(AppUtil.getHoraAtual());
+
+        NovoCadastro();
+
+    }
+
+    //Método do button para acessar a tela de cadastro
+    private void NovoCadastro() {
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent telaCadastro = new Intent(MainActivity.this, NovoCadastro.class);
+                startActivity(telaCadastro);
+
+            }
+        });
     }
 }
---
+++ Cadastro/app/src/main/java/app/modelo/cadastro/NovoCadastro.java
@@ -0,0 +1,36 @@
+package app.modelo.cadastro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NovoCadastro extends AppCompatActivity {
+
+    Button btnvoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_cadastro);
+
+        btnvoltar = findViewById(R.id.voltar);
+
+        BtnVoltar();
+
+    }
+
+    //Método para voltar á tela inicial
+    private void BtnVoltar() {
+        btnvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent telaInicial = new Intent(NovoCadastro.this, MainActivity.class);
+                startActivity(telaInicial);
+            }
+        });
+    }
+}

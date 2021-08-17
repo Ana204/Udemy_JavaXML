736c61ee892fc4c247e6123c8fbeda8ba6c5362b
ana204 <anagomes.lucia@hotmail.com>
2021-08-17 14:48:43 -0300

    Declando variaveis e criando método de recuperar senha

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/LoginActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/LoginActivity.java
@@ -3,12 +3,55 @@ package app.modelo.appclientevip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmailLogin, edtSenhaLogin;
+    CheckBox checkLembrar;
+    TextView recuperarSenha, politicaDePrivacidade;
+    Button btnAcessar, btnSejaVip;
+
+    boolean isFormularioLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initLogin();
+
+        recuperarsenhaEvento();
     }
+
+
+    private void initLogin() {
+
+        editEmailLogin.findViewById(R.id.editEmailLogin);
+        edtSenhaLogin.findViewById(R.id.edtSenhaLogin);
+        checkLembrar.findViewById(R.id.checkLembrar);
+        recuperarSenha.findViewById(R.id.recuperarSenha);
+        politicaDePrivacidade.findViewById(R.id.recuperarSenha);
+        btnAcessar.findViewById(R.id.btnAcessar);
+        btnSejaVip.findViewById(R.id.btnSejaVip);
+
+        isFormularioLogin = false;
+
+    }
+
+    private void recuperarsenhaEvento() {
+
+        recuperarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }

91eb24ac41bba66c275f543966271d4dbc34d61b
ana204 <anagomes.lucia@hotmail.com>
2021-08-19 17:33:39 -0300

    Layout para Clientes VIP

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/ClienteVipActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/ClienteVipActivity.java
@@ -1,8 +1,11 @@
-package app.modelo.appclientevip;
+package app.modelo.appclientevip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import app.modelo.appclientevip.R;
 
 public class ClienteVipActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class ClienteVipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_vip);
+    }
+
+    public void pessoaFisica(View view) {
     }
 }

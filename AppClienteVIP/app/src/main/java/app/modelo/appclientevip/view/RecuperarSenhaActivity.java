fcc56199276b4788a331217c35ef3d33fa31a0e3
ana204 <anagomes.lucia@hotmail.com>
2021-08-20 17:24:14 -0300

    Recuperar Senha Activity

---
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/RecuperarSenhaActivity.java
@@ -0,0 +1,16 @@
+package app.modelo.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.modelo.appclientevip.R;
+
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}

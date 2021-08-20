f53dca5068d092e52a6be66ffca0fcbe0b9ee333
ana204 <anagomes.lucia@hotmail.com>
2021-08-20 16:30:52 -0300

    Activity pessoa Juridica

---
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/PessoaJuridicaActivity.java
@@ -0,0 +1,16 @@
+package app.modelo.appclientevip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.modelo.appclientevip.R;
+
+public class PessoaJuridicaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pessoa_juridica);
+    }
+}

ce58c6a2b3f44fed8d2225c622bcdf57e2c56e1f
ana204 <anagomes.lucia@hotmail.com>
2021-09-02 17:00:20 -0300

    Class adapter para popular

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/api/ClienteAdapter.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/api/ClienteAdapter.java
@@ -1,36 +1,92 @@
 package app.modelo.appclientevip.api;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import app.modelo.appclientevip.R;
+import app.modelo.appclientevip.model.Cliente;
+
 public class ClienteAdapter extends RecyclerView.Adapter<ClienteAdapter.ViewHolder> {
-    @NonNull
-    @Override
-    public ClienteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+    private List<Cliente> aClientes; //Lista de clientes do adapter
+    private Context aContext;
+
+    public ClienteAdapter(List<Cliente> aClientes, Context aContext) {
+        this.aClientes = aClientes;
+        this.aContext = aContext;
     }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View clienteView = inflater.inflate(R.layout.activity_detalhes_clientes, parent, false);
+        ViewHolder viewHolder = new ViewHolder(clienteView);
+
+        return viewHolder;
+    }
+
+    //popular
     @Override
     public void onBindViewHolder(@NonNull ClienteAdapter.ViewHolder holder, int position) {
+
+        Cliente objDaLinha = aClientes.get(position);
+
+        TextView primeiroNome = holder.rvPrimeiroNome;
+        primeiroNome.setText(objDaLinha.getPrimeiroNome());
+
+        Button btnPessoa = holder.rvPessoa;
+        btnPessoa.setText(objDaLinha.isPessoaFisica() ? "CPF" : "CNPJ");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return aClientes.size();
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView rvPrimeiroNome;
+        public Button rvPessoa;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            rvPrimeiroNome = itemView.findViewById(R.id.rvPrimeiroNome);
+            rvPessoa = itemView.findViewById(R.id.rvPessoa);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+
+            int position = getAdapterPosition();
+
+            Cliente clienteSelecionado = aClientes.get(position);
+
+            if (position != RecyclerView.NO_POSITION){
+
+                Log.i(AppUtil.LOG_APP, "Cliente ID" + position + "Primeiro Nome: " +clienteSelecionado.getPrimeiroNome());
+
+                Toast.makeText(aContext, "Cliente ID" + position + "Primeiro Nome: " +clienteSelecionado.getPrimeiroNome(), Toast.LENGTH_LONG).show();
+            }
 
         }
     }
--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/ConsultarClientesActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/ConsultarClientesActivity.java
@@ -1,6 +1,39 @@
 package app.modelo.appclientevip.view;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.modelo.appclientevip.R;
+import app.modelo.appclientevip.api.ClienteAdapter;
+import app.modelo.appclientevip.model.Cliente;
 
 public class ConsultarClientesActivity extends Activity {
+
+    List<Cliente> clientes;
+    ClienteAdapter adapter;
+
+    Cliente obj;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_clientes);
+
+        clientes = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+
+            obj = new Cliente();
+
+            obj.setPrimeiroNome("Cliente " +i);
+            obj.setPessoaFisica(i % 2 == 0);
+
+            clientes.add(obj);
+        }
+
+        adapter = new ClienteAdapter(clientes, getApplicationContext());
+    }
 }

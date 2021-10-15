05540f75e244f7e49315a5b321b2bb439cb6c14c
ana204 <anagomes.lucia@hotmail.com>
2021-10-15 15:29:39 -0300

    instanciando variaveis

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
@@ -3,11 +3,17 @@ package app.modelo.appclientevip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import app.modelo.appclientevip.R;
 
 public class MeusDadosActivity extends AppCompatActivity {
 
+    EditText edtPrimeiroNome,edtSobrenome,edtCPF,
+            edtEmail, edtSenha,edtNomeCompleto, edtcnpj,razaoSocial,edtData;
+
+    CheckBox ckPessoaFisica,ckSimplesNacional,ckMei;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

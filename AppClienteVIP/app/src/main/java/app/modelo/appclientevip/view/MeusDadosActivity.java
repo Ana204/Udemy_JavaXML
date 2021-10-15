3ddd1b4f0ea5cd11af7c0922b785abb198c4cc6d
ana204 <anagomes.lucia@hotmail.com>
2021-10-15 16:06:32 -0300

    popular formulario

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
 
 import app.modelo.appclientevip.R;
 import app.modelo.appclientevip.api.AppUtil;
 import app.modelo.appclientevip.controller.ClienteController;
 import app.modelo.appclientevip.model.Cliente;
+import app.modelo.appclientevip.model.ClientePF;
+import app.modelo.appclientevip.model.ClientePJ;
 
 public class MeusDadosActivity extends AppCompatActivity {
 
@@ -36,12 +43,12 @@ public class MeusDadosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_dados);
 
-        initMeusDados();
         restaurarSharedPreferences();
 
+        initMeusDados();
+        popularFormulario();
         buttonVoltar();
     }
-
     private void initMeusDados() {
 
         edtPrimeiroNome = findViewById(R.id.edtPrimeiroNome);
@@ -79,5 +86,30 @@ public class MeusDadosActivity extends AppCompatActivity {
         preferences = getSharedPreferences(AppUtil.APP_PREFERENCIA, MODE_PRIVATE);
         isPessoaFisica = preferences.getBoolean("pessoaFisica", true) ;
         clienteID = preferences.getInt("UltimoID", -1);
+    }
+
+    private void popularFormulario() {
+
+        if (clienteID>=1){
+
+        }else{
+
+            FancyAlertDialog.Builder
+                    .with(MeusDadosActivity.this)
+                    .setTitle("ATENÇÃO")
+                    .setBackgroundColor(Color.parseColor("#303F9F"))  // for @ColorRes use setBackgroundColorRes(R.color.colorvalue)
+                    .setMessage("NÃO FOI POSSÍVEL RECUPERAR OS DADOS")
+                    .setNegativeBtnText("RETORNAR")
+                    .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  // for @ColorRes use setNegativeBtnBackgroundRes(R.color.colorvalue)
+                    .setAnimation(Animation.POP)
+                    .isCancellable(true)
+                    .setIcon(R.drawable.ic_star_border_black_24dp, View.VISIBLE)
+                    .onNegativeClicked(dialog -> {
+
+                        buttonVoltar();
+                    })
+                    .build();
+
+        }
     }
 }

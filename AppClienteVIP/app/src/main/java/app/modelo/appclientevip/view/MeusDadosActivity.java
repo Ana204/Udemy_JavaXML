15b25873add7b77f230553e2f6c6d986da8468fa
ana204 <anagomes.lucia@hotmail.com>
2021-10-15 15:43:08 -0300

    button voltar

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
@@ -2,7 +2,9 @@ package app.modelo.appclientevip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -24,6 +26,7 @@ public class MeusDadosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meus_dados);
 
         initMeusDados();
+        buttonVoltar();
     }
 
     private void initMeusDados() {
@@ -42,5 +45,16 @@ public class MeusDadosActivity extends AppCompatActivity {
         ckMei = findViewById(R.id.ckMei);
         voltarBtn = findViewById(R.id.voltarBtn);
 
+    }
+
+    private void buttonVoltar() {
+
+        voltarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MeusDadosActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

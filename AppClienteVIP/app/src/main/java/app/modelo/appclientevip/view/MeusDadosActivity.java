29e499e8d4149340cde99e2fde48427f5fc83b33
ana204 <anagomes.lucia@hotmail.com>
2021-10-15 15:39:52 -0300

    pegando id da tag

--- AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
+++ AppClienteVIP/app/src/main/java/app/modelo/appclientevip/view/MeusDadosActivity.java
@@ -22,5 +22,25 @@ public class MeusDadosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_dados);
+
+        initMeusDados();
+    }
+
+    private void initMeusDados() {
+
+        edtPrimeiroNome = findViewById(R.id.edtPrimeiroNome);
+        edtSobrenome = findViewById(R.id.edtSobrenome);
+        edtCPF = findViewById(R.id.edtCPF);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        edtNomeCompleto = findViewById(R.id.edtNomeCompleto);
+        edtcnpj = findViewById(R.id.edtcnpj);
+        razaoSocial = findViewById(R.id.razaoSocial);
+        edtData = findViewById(R.id.edtData);
+        ckPessoaFisica = findViewById(R.id.ckPessoaFisica);
+        ckSimplesNacional = findViewById(R.id.ckSimplesNacional);
+        ckMei = findViewById(R.id.ckMei);
+        voltarBtn = findViewById(R.id.voltarBtn);
+
     }
 }

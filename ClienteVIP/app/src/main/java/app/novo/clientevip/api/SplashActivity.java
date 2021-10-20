ffdc38b5e05324df060de5aff45886e88f13961e
ana204 <anagomes.lucia@hotmail.com>
2021-10-20 16:35:15 -0300

    tela SplashActivity

---
+++ ClienteVIP/app/src/main/java/app/novo/clientevip/api/SplashActivity.java
@@ -0,0 +1,67 @@
+package app.novo.clientevip.api;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.novo.clientevip.R;
+
+public class SplashActivity extends AppCompatActivity {
+/*
+    private SharedPreferences preferences;
+    boolean LembrarSenha = false;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        telaSplash();
+/*
+        salvarSharedPreferences();
+        restaurarSharedPreferences();*/
+
+
+    }
+
+    private void telaSplash() {
+
+        new Handler().postDelayed(() -> {
+
+            Intent intent;
+
+/*            if (LembrarSenha) {
+
+                intent = new Intent(SplashActivity.this, MainActivity.class);
+
+            } else {
+                intent = new Intent(SplashActivity.this, LoginActivity.class);
+
+            }*/
+            intent = new Intent(SplashActivity.this, LoginActivity.class);
+
+            startActivity(intent);
+            finish();
+            return;
+
+        }, AppUtil.TIME_SPLASH);
+    }
+
+/*    private void salvarSharedPreferences() {
+
+        preferences = getSharedPreferences(AppUtil.APP_PREFERENCIA, MODE_PRIVATE);
+        SharedPreferences.Editor dados = preferences.edit();
+    }
+
+    private void restaurarSharedPreferences() {
+
+        preferences = getSharedPreferences(AppUtil.APP_PREFERENCIA, MODE_PRIVATE);
+
+        LembrarSenha = preferences.getBoolean("loginAutomatico", false);
+
+
+    }*/
+}

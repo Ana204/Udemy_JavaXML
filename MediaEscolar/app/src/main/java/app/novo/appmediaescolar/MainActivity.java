cb2fc080ac3a464a7c19b9ed34dca66988842123
ana204 <anagomes.lucia@hotmail.com>
2021-12-02 12:57:44 -0300

    somando duas notas e dividindo o resultado por 2, dando assim reprovado ou aprovado

--- MediaEscolar/app/src/main/java/app/novo/appmediaescolar/MainActivity.java
+++ MediaEscolar/app/src/main/java/app/novo/appmediaescolar/MainActivity.java
@@ -17,15 +17,20 @@ import app.novo.appmediaescolar.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -39,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "MÉDIA ESCOLAR", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -47,20 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sair) {
+
+            finish();
             return true;
         }
 
--- MediaEscolar/app/src/main/java/app/novo/appmediaescolar/SecondFragment.java
+++ MediaEscolar/app/src/main/java/app/novo/appmediaescolar/SecondFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,13 @@ import app.novo.appmediaescolar.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+
+    TextView media,situacao;
+    EditText edtMateria, edtNotaTrabalho, edtNotaProva;
+    Button calcular;
+
+    double notaProva,notaTrabalho, dMedia;
+    String resultado;
 
     @Override
     public View onCreateView(
@@ -30,9 +40,29 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        edtMateria = view.findViewById(R.id.edtMateria);
+        edtNotaProva = view.findViewById(R.id.edtNotaProva);
+        edtNotaTrabalho = view.findViewById(R.id.edtNotaTrabalho);
+        media = view.findViewById(R.id.media);
+        situacao = view.findViewById(R.id.situacao);
+        calcular = view.findViewById(R.id.calcular);
+
         binding.calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                notaProva = Double.parseDouble(edtNotaProva.getText().toString());
+                notaTrabalho = Double.parseDouble(edtNotaTrabalho.getText().toString());
+
+                dMedia = (notaProva + notaTrabalho)/2;
+                media.setText(String.valueOf(dMedia));
+
+                if (dMedia >= 6) {
+                    situacao.setText("APROVADO");
+                } else {
+                    situacao.setText("REPROVADO");
+                }
 
                 Toast.makeText(getContext(), "BUTTON CALCULAR CLICADO", Toast.LENGTH_LONG).show();
             }

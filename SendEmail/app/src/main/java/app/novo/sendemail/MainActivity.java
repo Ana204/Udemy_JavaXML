d18d8e04b3aa6223b7c7a476f1c6c153b144cf8f
ana204 <anagomes.lucia@hotmail.com>
2021-12-03 17:30:59 -0300

    Button enviar, class intent

--- SendEmail/app/src/main/java/app/novo/sendemail/MainActivity.java
+++ SendEmail/app/src/main/java/app/novo/sendemail/MainActivity.java
@@ -2,13 +2,69 @@ package app.novo.sendemail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtPara;
+    EditText edtEnviarPara,addAssunto,addMensagem;
+    Button btnEnviar;
+
+    Intent intent;
+    String emailDestinatario, assunto, mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initForm();
+
+        buttonEnviar();
+    }
+
+
+
+    private void initForm() {
+
+        txtPara = findViewById(R.id.txtPara);
+        edtEnviarPara = findViewById(R.id.edtEnviarPara);
+        addAssunto = findViewById(R.id.addAssunto);
+        addMensagem = findViewById(R.id.addMensagem);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailDestinatario});
+        intent.putExtra(Intent.EXTRA_SUBJECT, assunto);
+        intent.putExtra(Intent.EXTRA_TEXT, mensagem);
+        intent.setType("message/rfc822");
+    }
+
+    private void buttonEnviar() {
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                ProcessarFormulario();
+                Toast.makeText(MainActivity.this, "Mensagem enviada com sucesso", Toast.LENGTH_LONG).show();
+                edtEnviarPara.setText("");
+                addAssunto.setText("");
+                addMensagem.setText("");
+            }
+        });
+    }
+
+    private void ProcessarFormulario() {
+
+        emailDestinatario = edtEnviarPara.getText().toString();
+        assunto = addAssunto.getText().toString();
+        mensagem = addMensagem.getText().toString();
     }
 }

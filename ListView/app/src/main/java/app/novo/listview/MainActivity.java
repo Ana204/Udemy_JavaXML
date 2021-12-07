6f2fc6a048e8f77706eb3219b895ca0ccddb7612
ana204 <anagomes.lucia@hotmail.com>
2021-12-07 17:23:53 -0300

    adapter listView

--- ListView/app/src/main/java/app/novo/listview/MainActivity.java
+++ ListView/app/src/main/java/app/novo/listview/MainActivity.java
@@ -1,14 +1,34 @@
 package app.novo.listview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+   static final String[] cidades = new String[]{
+            "Campo Grande - MS",
+            "São Paulo - SP",
+            "Brasília - DF",
+            "Rio de Janeiro - RJ",
+            "Salvador - BH",
+            "Belo Horizonte - MG",
+            "Goiânia - GA",
+            "Belém - PR",
+            "João Pessoa - PB",
+            "Maringá - PR",
+            "Praia Grande - SP",
+            "Dourados - MS",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, cidades));
+
+        ListView cidadesListView = getListView();
     }
 }

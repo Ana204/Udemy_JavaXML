c89483be51de38a812053cbfbf6c13483380e117
ana204 <anagomes.lucia@hotmail.com>
2021-12-07 17:33:23 -0300

    adapter listView

--- ListView/app/src/main/java/app/novo/listview/MainActivity.java
+++ ListView/app/src/main/java/app/novo/listview/MainActivity.java
@@ -2,8 +2,11 @@ package app.novo.listview;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -25,10 +28,17 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, cidades));
 
         ListView cidadesListView = getListView();
+
+        cidadesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, "CIDADE - " + " " +cidades[i], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

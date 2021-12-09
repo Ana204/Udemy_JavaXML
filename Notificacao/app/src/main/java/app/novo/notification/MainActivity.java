0c618ce61764dfe1515afc6a7737a450fda97d79
ana204 <anagomes.lucia@hotmail.com>
2021-12-09 13:39:42 -0300

    recebendo notificação

--- Notificacao/app/src/main/java/app/novo/notification/MainActivity.java
+++ Notificacao/app/src/main/java/app/novo/notification/MainActivity.java
@@ -1,7 +1,14 @@
 package app.novo.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String mensagem = "50% de desconto";
+        String titulo = "MEGA PROMOÇÃO";
+
+        notificarUsuario(mensagem, titulo);
+    }
+
+    private void notificarUsuario(String mensagem, String titulo) {
+
+        NotificationCompat.Builder notificacao = new NotificationCompat.Builder(getBaseContext());
+
+        notificacao.setContentTitle(titulo);
+        notificacao.setContentText(mensagem);
+        notificacao.setPriority(Notification.PRIORITY_HIGH);
+
+        notificacao.setLargeIcon(BitmapFactory.decodeResource(getBaseContext().getResources(), R.mipmap.notification));
+
+        notificacao.setSmallIcon(R.drawable.comment);
+
+        Intent intent = new Intent(getBaseContext(), NotificacaoActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getBaseContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        notificacao.setAutoCancel(true);
+        notificacao.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(900, notificacao.build());
+
     }
 }

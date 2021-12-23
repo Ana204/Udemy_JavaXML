106fbdd13effcf3ac809bd44b398267ac2bcea21
ana204 <anagomes.lucia@hotmail.com>
2021-12-23 15:33:13 -0300

    metodo onMessageReceived

--- Notificacao/app/src/main/java/app/novo/notification/push/PushFireBaseMessagingService.java
+++ Notificacao/app/src/main/java/app/novo/notification/push/PushFireBaseMessagingService.java
@@ -3,11 +3,16 @@ package app.novo.notification.push;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class PushFireBaseMessagingService extends FirebaseMessagingService {
 
     public void onNewToken(String s){
         super.onNewToken(s);
-        Log.e("NEW_TOKEN", s);
+        Log.i("App_Push", "Novo Token" + s);
+    }
+
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
     }
 }

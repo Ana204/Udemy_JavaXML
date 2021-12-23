68ea2ef8d24c20f2912a19fef29b218832520ce0
ana204 <anagomes.lucia@hotmail.com>
2021-12-23 15:30:20 -0300

    metodo OnNewToken

--- Notificacao/app/src/main/java/app/novo/notification/push/PushFireBaseMessagingService.java
+++ Notificacao/app/src/main/java/app/novo/notification/push/PushFireBaseMessagingService.java
@@ -1,7 +1,13 @@
 package app.novo.notification.push;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class PushFireBaseMessagingService extends FirebaseMessagingService {
 
+    public void onNewToken(String s){
+        super.onNewToken(s);
+        Log.e("NEW_TOKEN", s);
+    }
 }

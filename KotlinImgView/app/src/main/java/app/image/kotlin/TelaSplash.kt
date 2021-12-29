f2dc86e52c73828b603bbc7b2211f8291a4e2f78
ana204 <anagomes.lucia@hotmail.com>
2021-12-29 17:15:01 -0300

    TelaSplash

--- KotlinImgView/app/src/main/java/app/image/kotlin/TelaSplash.kt
+++ KotlinImgView/app/src/main/java/app/image/kotlin/TelaSplash.kt
@@ -1,11 +1,20 @@
 package app.image.kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
 
 class TelaSplash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_tela_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@TelaSplash, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }

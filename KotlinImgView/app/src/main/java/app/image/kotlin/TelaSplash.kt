27141ddbb1b01d992ba67522f3abfae391d0461b
ana204 <anagomes.lucia@hotmail.com>
2021-12-29 16:41:59 -0300

    TelaSplash layout

---
+++ KotlinImgView/app/src/main/java/app/image/kotlin/TelaSplash.kt
@@ -0,0 +1,11 @@
+package app.image.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaSplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_splash)
+    }
+}

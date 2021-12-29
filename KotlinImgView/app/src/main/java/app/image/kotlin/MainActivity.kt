bcb6f3d91eede6aabd3c07b7019f589d9dc967e6
ana204 <anagomes.lucia@hotmail.com>
2021-12-29 16:15:38 -0300

    image

--- KotlinImgView/app/src/main/java/app/image/kotlin/MainActivity.kt
+++ KotlinImgView/app/src/main/java/app/image/kotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package app.image.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageLogo = findViewById<ImageView>(R.id.imageLogo)
+
+        val drawableID = R.drawable.kotlin_icon
+
+        var compararID = drawableID;
+
+        imageLogo.setImageResource(drawableID)
+
+        val buttonClique = findViewById<Button>(R.id.buttonClique)
+
+        buttonClique?.setOnClickListener {
+
+            compararID =
+                if (compararID == R.drawable.java_logo)
+                    R.drawable.kotlin_icon
+                else R.drawable.java_logo
+
+            imageLogo.setImageResource(compararID)
+        }
     }
 }
